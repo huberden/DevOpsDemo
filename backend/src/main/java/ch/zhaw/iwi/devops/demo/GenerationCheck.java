0b6a3e8e449d87bb5e1bf6d64d9066de523b19eb
huberden <91844408+huberden@users.noreply.github.com>
2023-04-04 16:57:42 -0300

    New Test

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/GenerationCheck.java
@@ -0,0 +1,22 @@
+package ch.zhaw.iwi.devops.demo;
+
+public class GenerationCheck {
+
+    public String Checking(int jahrgang){
+        if (jahrgang >= 2011 && jahrgang <= 2025){
+            return "Generation Alpha";
+        } else if (jahrgang >= 1997 && jahrgang <= 2010){
+            return "Generation Z";
+        } else if (jahrgang >= 1981 && jahrgang <= 1996){
+            return "Generation Y";
+        } else if (jahrgang >= 1965 && jahrgang <= 1980){
+            return "Generation X";
+        } else if (jahrgang >= 1946 && jahrgang <= 1964){
+            return "Generation Boomer";
+        } else if (jahrgang >= 1928 && jahrgang <= 1945){
+            return "Generation Silent";
+        } else if (jahrgang >= 2026 && jahrgang <= 2040){
+            return "Generation Beta";
+        } else {
+            return "unbestimmt";
+}}}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/GenerationCheckTest.java
@@ -0,0 +1,72 @@
+package ch.zhaw.iwi.devops.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GenerationCheckTest {
+
+    @Test
+
+    public void GenerationCheckAlpha(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Alpha", ac.Checking(2011));
+
+    }
+
+    @Test
+
+    public void GenerationCheckZ(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Z", ac.Checking(2000));
+    }
+    
+    @Test
+
+    public void GenerationCheckY(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Y", ac.Checking(1992));
+    }
+
+    @Test
+
+    public void GenerationCheckX(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation X", ac.Checking(1968));
+    }
+
+    @Test
+
+    public void GenerationCheckBoomer(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Boomer", ac.Checking(1960));
+    }
+
+    @Test
+
+    public void GenerationCheckSilent(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Silent", ac.Checking(1945));
+    }
+
+    @Test
+
+    public void GenerationCheckBeta(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("unbestimmt", ac.Checking(2043));
+    }
+
+    @Test
+
+    public void GenerationCheckBeta2(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("Generation Beta", ac.Checking(2040));
+    }
+
+    @Test
+
+    public void GenerationCheckNone(){
+        GenerationCheck ac = new GenerationCheck();
+        assertEquals("unbestimmt", ac.Checking(30000));
+    }
+}
